cc1ccb2e5f7c993489be11dcca229884c55da147
AnaG <initiana794@gmail.com>
2023-10-27 18:30:11 +0200

    Creación Clase Familie

---
+++ Tarea3/Aufgabe2a/src/Familie.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Familie {
+    private ArrayList mitglieder;
+
+    public Familie(String vater, String mutter) {
+        //Aquí va el código
+    }
+
+    public void addKind(String kind) {
+        //Código
+    }
+
+    public enum Familienmitglied {
+        //Código
+    }
+    //método getMitglied(...)
+}
