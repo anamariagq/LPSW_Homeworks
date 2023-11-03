62dcba1e09a75d303d127d28953d532629718ff4
AnaG <initiana794@gmail.com>
2023-11-03 11:21:59 +0100

    ArrayList, Constructor de la Clase, Métodos addKind y getMitglied, Enum Familienmitglied y Creación clase Programm

--- Tarea3/Aufgabe2a/src/Familie.java
+++ Tarea3/Aufgabe2a/src/Familie.java
@@ -1,17 +1,63 @@
+import java.io.IOException;
 import java.util.ArrayList;
 public class Familie {
-    private ArrayList mitglieder;
+    private ArrayList<String> mitglieder;
 
-    public Familie(String vater, String mutter) {
-        //Aquí va el código
+    public Familie(String vater, String mutter) {//aquí va el código
+        if(vatter == null || vater.isEmpty() || mutter == null || mutter.isEmpty()){
+            throw new IllegalArgumentException("No empty parameters allowed. For no Vater or Mutter, please enter an Empty Space");
+        }
+        mitglieder = new ArrayList<>();
+        mitglieder.add(vater);//necesario??
+        mitglieder.add(mutter);
+        //la ki hace al comienzo una IllegalArgumentException y cierra el programa
     }
 
-    public void addKind(String kind) {
-        //Código
+
+    public void addKind(String kind) {//Código
+        if(vatter == null || vater.isEmpty() || mutter == null || mutter.isEmpty()){
+            throw new IllegalArgumentException("No empty parameters allowed. For no Children, please enter an Empty Space");
+        }
+      mitglieder.add(kind);
     }
 
-    public enum Familienmitglied {
-        //Código
+    public void getMitglied(Familienmitglied mitglied){
+        if(mitglied == Familienmitglied.VATER){
+            return mitglied.get(0);
+        } else if (mitglied == Familienmitglied.MUTTER) {
+            return mitglied.get(1);
+        } else if (mitglied == Familienmitglied.KIND) {
+            if(mitglied.size()>2){
+                return mitglied.get(", ");
+            } else{
+                return ".";
+            }
+        }
+        return ".";
     }
-    //método getMitglied(...)
+
 }
+ /*for (int i = 0; i < mitglieder.size(); i++) {
+            try {
+                mitglieder.add(0, "vater");
+                mitglieder.add(1, "mutter");
+            } catch(IllegalArgumentException e){
+                System.out.println("There was a problem with adding your statement.");
+
+            }finally{
+                System.out.println("Die Familien Mitglieder sind " + mitglieder);
+            }
+        }
+    }*/
+ /*for(int i= 2; i < mitglieder.size(); i++){
+           try {
+               mitglieder.add("kind" + i);
+           }catch (IllegalArgumentException e){
+               System.out.println("Die Familien Mitglieder sind " + mitglieder);
+           }finally {
+               System.out.println("Die Familien Mitglieder sind " + kind + ",");
+           }// esto se podría hacer en una operación separada.
+           //primero guardar y definir los elememtos. Posteriormente hacer
+           //otras funciones
+       }*/
+
---
+++ Tarea3/Aufgabe2a/src/Familienmitglied.java
@@ -0,0 +1,8 @@
+public enum Familienmitglied {
+    VATER("vater"), MUTTER("mutter"), KIND("kind");
+    private final String mitglied;
+    public Familienmitglied(String _mitglied){
+        this.mitglied = _mitglied;
+    }
+
+}
